7588957279134404c3eba7ec2984b990c61c701e
GuiBourdin <50623880+guibourdin@users.noreply.github.com>
2020-06-24 13:55:03 -0400

    Update UVPread_persample.m

--- UVPread_persample.m
+++ UVPread_persample.m
@@ -13,21 +13,7 @@
 
 offset_depth=1.2;
 %%
-
 %for i = 1:width(t), if iscell(t.(i)), t.(i) = cell2mat(t.(i)); end, end
-
-% prompt = {'Enter max depth'};%
-% dlg_title = 'Input';
-% num_lines = 1;
-% defaultans = {'200'};
-% maxdepth = inputdlg(prompt,dlg_title,num_lines,defaultans);
-% maxdepth=str2num(maxdepth{1});
-prompt = {'Enter depth bin size (m)'};%
-dlg_title = 'Input';
-num_lines = 1;
-defaultans = {'200'};
-sz_depthbin = inputdlg(prompt,dlg_title,num_lines,defaultans);
-sz_depthbin = str2double(sz_depthbin{1});
 
 A=dir('*tsv');
 filenames={A.name};
@@ -37,13 +23,35 @@ fprintf('Select the particle base\n')
 
 load(file)  % load tha particles data base in which volume per depth intervals are storred
 samplebase={base(:).profile};
+
+% prompt = {'Enter max depth'};%
+% dlg_title = 'Input';
+% num_lines = 1;
+% defaultans = {'200'};
+% maxdepth = inputdlg(prompt,dlg_title,num_lines,defaultans);
+% maxdepth=str2num(maxdepth{1});
+prompt = {'Enter depth bin size above MLD (m)', 'Input MLD (enter "no" to use default = 150 m)', 'Enter depth bin size below MLD (m)'};%
+dlg_title = 'Input';
+num_lines = 1;
+defaultans = {'50', 'Yes', '300'};
+sz_depthbin = inputdlg(prompt,dlg_title,num_lines,defaultans);
+up_sz_depthbin = str2double(sz_depthbin{1});
+down_sz_depthbin = str2double(sz_depthbin{3});
+if contains(sz_depthbin{2}, {'yes', 'Yes', 'YES', 'true', 'True', 'TRUE'})
+    [MLDfile,~] = uigetfile('*.csv');
+    MLD = readtable(MLDfile);
+else
+    MLD = table(fullfile({base.profile}'), repmat(150, size(base,1), 1), 'VariableNames', {'profile', 'mld'});
+    fprintf('Default MLD used: 150 m')
+end
+
 %samplebase=samplebase';
 %% caution this assumes that the same UVP was used for an entire project (not anymore= corrected )
 expo=[base(:).exp];	
 aa=[base(:).aa];
 %pixelsize=aa.^(expo); %
 pixelsize=[base(:).PixelSize]; %in mm
-pixelsize=pixelsize.^0.5; % in mm2
+% pixelsize=pixelsize.^0.5; % in mm2
 
 
 % sampleheaders=readtable('allsampleheaders.xlsx');
@@ -75,7 +83,6 @@ for i=1:m
     sample=unique(S.sample_id);
     fprintf('Sorting %s ... ', cell2mat(sample))
     %for i = 1:width(S), if iscell(t.(i)), t.(i) = cell2mat(t.(i)); end, end
-    
     %% cleaning anotation hierarchy
 %     [n,no_use]=size(S);
     S.object_annotation_hierarchy = strrep(S.object_annotation_hierarchy,'-','_');
@@ -149,27 +156,33 @@ for i=1:m
     %pixelsize=0.174^(0.5);
     %%
     I = find(contains(samplebase,temp));
-    volumes=base(I(1)).histnb.data.SampledVolume_L_;   %basetot(1,I).hisnb(:,3).*basetot(1,I).volimg0;   
-    depthUVP=base(I(1)).histnb.data.Depth_m_; %basetot(1,I).hisnb(:,1);
+    volumes = base(I(1)).histnb.data.SampledVolume_L_;   %basetot(1,I).hisnb(:,3).*basetot(1,I).volimg0;   
+    depthUVP = base(I(1)).histnb.data.Depth_m_; %basetot(1,I).hisnb(:,1);
 %    if isempty(basetot(I).zoopuvp5)==0;
-%       pixelsize=basetot(I).zoopuvp5.pixel;
+%       pixelsize = basetot(I).zoopuvp5.pixel;
 %    end
-    base_Zooscan(i).pixelsize=pixelsize(I(1));
-    base_Zooscan(i).tot = table([],[],{},{},{},[],[],{},{},{},{},{},{},{},...
-        'VariableNames',{'higher_bin_depth','lower_bin_depth','vol','conver',...
+    base_Zooscan(i).pixelsize = pixelsize(I(1));
+    base_Zooscan(i).tot = table(NaN(ceil(max(depthUVP)/up_sz_depthbin),1),...
+        NaN(ceil(max(depthUVP)/up_sz_depthbin),1), cell(ceil(max(depthUVP)/up_sz_depthbin),1),...
+        cell(ceil(max(depthUVP)/up_sz_depthbin),1), cell(ceil(max(depthUVP)/up_sz_depthbin),1),...
+        NaN(ceil(max(depthUVP)/up_sz_depthbin),1), NaN(ceil(max(depthUVP)/up_sz_depthbin),1),...
+        cell(ceil(max(depthUVP)/up_sz_depthbin),1), cell(ceil(max(depthUVP)/up_sz_depthbin),1),...
+        cell(ceil(max(depthUVP)/up_sz_depthbin),1), cell(ceil(max(depthUVP)/up_sz_depthbin),1),...
+        cell(ceil(max(depthUVP)/up_sz_depthbin),1), cell(ceil(max(depthUVP)/up_sz_depthbin),1),...
+        cell(ceil(max(depthUVP)/up_sz_depthbin),1), 'VariableNames',{'higher_bin_depth','lower_bin_depth','vol','conver',...
         'depthstrata','totvol','totconver','object_annotation_hierarchy',...
         'depth','major','minor','area_exc','area','perimferet'});
     % bin total depth
-    bin_lower_depth = sz_depthbin+2.5;
+    bin_higher_depth = 0;
+    if up_sz_depthbin <= MLD.mld(i)
+        bin_lower_depth = up_sz_depthbin + 2.5;
+    else
+        bin_lower_depth = down_sz_depthbin + 2.5;
+    end
     pp = 1;
-    while any(pp==1 | bin_lower_depth <= max(depthUVP))
-        if bin_lower_depth-sz_depthbin == 2.5
-            bin_higher_depth = bin_lower_depth-sz_depthbin-2.5;
-        else
-            bin_higher_depth = bin_lower_depth-sz_depthbin;
-        end
+    while bin_higher_depth < max(depthUVP)
         I = depthUVP > bin_higher_depth & depthUVP <= bin_lower_depth;
-    %     return
+%         return
         base_Zooscan(i).tot.higher_bin_depth(pp) = bin_higher_depth;
         base_Zooscan(i).tot.lower_bin_depth(pp) = max(depthUVP(I));
         base_Zooscan(i).tot.vol{pp} = volumes(I)/1000; %in m3
@@ -178,10 +191,10 @@ for i=1:m
 
         base_Zooscan(i).tot.totvol(pp) = nansum(volumes(I)/1000); %in m3
         base_Zooscan(i).tot.totconver(pp) = 1/nansum(volumes(I)/1000);
-    %     return
-    %     base_Zooscan(i).tot.Scanfilename=
-    %     base_Zooscan(i).FracIds=unique(S.acq_id);
-    %     [nfrac, no_use]=size(base_Zooscan(i).FracIds);
+%         return
+%         base_Zooscan(i).tot.Scanfilename=
+%         base_Zooscan(i).FracIds=unique(S.acq_id);
+%         [nfrac, no_use]=size(base_Zooscan(i).FracIds);
         S.object_depth_max = S.object_depth_max+offset_depth;
         I = S.object_depth_max > bin_higher_depth & S.object_depth_max <= bin_lower_depth;
         %base_Zooscan(i).tot.Fracmin{pp} = unique(S.acq_min_mesh(I));
@@ -190,10 +203,10 @@ for i=1:m
         %base_Zooscan(i).tot.Scanned_Objects{pp} =
         %base_Zooscan(i).tot.Resolution{pp} = unique(S.process_img_resolution(I));
 
-%         base_Zooscan(i).tot.object_annotation_hierarchy=unique(S.object_annotation_hierarchy(I));
+%         base_Zooscan(i).tot.object_annotation_hierarchy = unique(S.object_annotation_hierarchy(I));
         Idlist = unique([Idlist; S.object_annotation_hierarchy(I)]);
         base_Zooscan(i).tot.object_annotation_hierarchy{pp} = S.object_annotation_hierarchy(I);
-        base_Zooscan(i).tot.depth{pp} = S.object_depth_max(I);%,S.object_depth_max
+        base_Zooscan(i).tot.depth{pp} = S.object_depth_max(I); %,S.object_depth_max
         %base_Zooscan(i).tot.object_annotation_hierarchy{pp} = S.object_annotation_hierarchy;
         base_Zooscan(i).tot.major{pp} = S.object_major(I)*base_Zooscan(i).pixelsize; %object_perimmajor
         base_Zooscan(i).tot.minor{pp} = S.object_minor(I)*base_Zooscan(i).pixelsize;
@@ -201,9 +214,19 @@ for i=1:m
         base_Zooscan(i).tot.area{pp} = S.object_area(I)*(base_Zooscan(i).pixelsize^2);    %object__area
         base_Zooscan(i).tot.perimferet{pp} = S.object_feret(I)*base_Zooscan(i).pixelsize;   % object_perimferet   %object_feretareaexc
         %base_Zooscan(i).tot.conver{pp} =  str2num(cell2mat(base_Zooscan(i).d.Fracnb))./base_Zooscan(i).Vol; %per cubic meter
-        bin_lower_depth = bin_lower_depth + sz_depthbin;
+        if bin_higher_depth <= MLD.mld(i)
+            bin_lower_depth = bin_lower_depth + up_sz_depthbin;
+        else
+            bin_lower_depth = bin_lower_depth + down_sz_depthbin;
+        end
+        if bin_higher_depth <= MLD.mld(i)
+            bin_higher_depth = bin_lower_depth - up_sz_depthbin;
+        else
+            bin_higher_depth = bin_lower_depth - down_sz_depthbin;
+        end
         pp = pp + 1;
     end
+    base_Zooscan(i).tot(isnan(base_Zooscan(i).tot.higher_bin_depth),:) = [];
     fprintf('done\n')
     waitbar(i/m)
 end
@@ -215,16 +238,16 @@ close(h)
 %% now working on spectra
 %% -------------- setting for calculus of the size spectra ------------------------------
 % Area, Minor and Major are in mm in base_zooscan
-smin=0.000000000001;     %set the lower limit of the biovolume spectra that will be calculated
-smax=10000;      %set the upper limit of the biovolume spectra that will be calculated
-k=2^(1/4);      %set logarithmic base used to calculate bins of the size spectra
+smin = 0.000000000001;     %set the lower limit of the biovolume spectra that will be calculated
+smax = 10000;      %set the upper limit of the biovolume spectra that will be calculated
+k = 2^(1/4);      %set logarithmic base used to calculate bins of the size spectra
 %according to platt & denman's theory (1978). scaling
 %exponent set to 0.25.
-uu=1; % change here the first size class for the regression (and put option = 1 for performing on the spectra from uu to end)
+uu = 1; % change here the first size class for the regression (and put option = 1 for performing on the spectra from uu to end)
 %zoo_groups = sort(unique(zoo_groups));
 %% starting to process the base
-[~,m]=size(base_Zooscan);
-zoo_groups=Idlist;
+[~,m] = size(base_Zooscan);
+zoo_groups = Idlist;
 %return
 nsamp = 0;
 for i=1:m
@@ -232,12 +255,12 @@ for i=1:m
     for j = 1:size(base_Zooscan(i).tot,1)
         nsamp = nsamp + 1;
         %[nfrac, no_use]=size(base_Zooscan(i).FracIds);
-        fracnb=1;
+        fracnb = 1;
         conver1 = base_Zooscan(i).tot.conver{j};
         depth = base_Zooscan(i).tot.depth{j};
         depthstrata = base_Zooscan(i).tot.depthstrata{j};
 
-        depthstrata=depthstrata(1:size(conver1,1));
+        depthstrata = depthstrata(1:size(conver1,1));
 %         conver2=NaN(size(depth,1),1);
         %% caution only works with 5m depth intervals
 %         toto = 0.5+depthstrata/5;
@@ -249,9 +272,10 @@ for i=1:m
         conver2 = conver1(toto2);
         base_Zooscan(i).tot.conver{j} = conver2;
         base_Zooscan(i).tot.converorig{j} = conver1;
-        base_Zooscan(i).tot.conver{j}=conver2;
+        base_Zooscan(i).tot.conver{j} = conver2;
     end
-    [base_Zooscan, SStot] = process_abundances_spectres_multiples_UVP(base_Zooscan(i).tot,base_Zooscan,smin,smax,k,uu,zoo_groups,i,fracnb);
+    [base_Zooscan, SStot] = process_abundances_spectres_multiples_UVP(base_Zooscan(i).tot,...
+        base_Zooscan, smin, smax, k, uu, zoo_groups, i, fracnb);
     fprintf('done\n')
 end
 
